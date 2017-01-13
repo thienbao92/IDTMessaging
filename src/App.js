19df6286876438d0132d09dbccf8898afda33346
bobo <bao.vu@oneup.fi>
2017-01-13 17:46:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.scss';
+import NavContainer from './components/Nav/NavContainer';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
